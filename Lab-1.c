cf06362ab43f811207a5335c5a58c7c3432f8cce
arda30170 <ardakoca957@gmail.com>
2026-04-01 23:17:30 +0300

    add lab-1 quesitons

---
+++ Lab-1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+   /* Quesiton-1 */
+   printf("Name: Arda \n");
+   printf("Surname: Koca \n");
+   printf("Hometown: Ankara \n");
+   
+   /* Quesiton-2 */
+   printf("*********** \n");
+   printf("* \t  * \n");
+   printf("* \t  * \n");
+   printf("* \t  * \n");
+   printf("* \t  * \n");
+   printf("* \t  * \n");
+   printf("* \t  * \n");
+   printf("* \t  * \n");
+   printf("* \t  * \n");
+   printf("* \t  * \n");
+
+   /* Quesiton-3 */
+   int a = 5;
+   int b = 3;
+   printf("Value of a variable is: %d \n", a);
+   printf("Value of b variables is: %d", b);
+   
+   /* Quesiton-4 */
+   float score = 4;
+   printf("The letter grade is A, and it's coefficient is: %f", score);
+   return 0;
+}
